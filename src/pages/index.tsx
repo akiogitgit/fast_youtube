551dac562c3699295e0330ba2a19c09fd72b8c42
Akio Tanaka <akimaron2828@icloud.com>
2022-05-02 08:43:38 +0900

    chore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,8 @@ interface videos {}
 
 const Home: NextPage = () => {
   const { data: session } = useSession()
-  // const apikey = String(process.env.NEXT_PUBLIC_YOUTUBE_APIKEY)
-  const apikey = String(process.env.NEXT_PUBLIC_YOUTUBE_APIKEY2)
+  const apikey = String(process.env.NEXT_PUBLIC_YOUTUBE_APIKEY)
+  // const apikey = String(process.env.NEXT_PUBLIC_YOUTUBE_APIKEY2)
   const channelID = 'UCBL4qbfyteUA-KGj3_9G1LA'
   const playListUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUacLlgUxCnIUegABaDTslyg&maxResults=15&key=${apikey}`
 
