ab1783668b036231b00f4f93247767ec49b760ec
Akio Tanaka <akimaron2828@icloud.com>
2022-05-02 11:27:51 +0900

    feat: チャンネルIDから動画を取得

--- src/pages/youtubeTest.tsx
+++ src/pages/youtubeTest.tsx
@@ -1,10 +1,114 @@
 import { NextPage } from 'next'
 import Link from 'next/link'
+import { useCallback, useEffect, useState } from 'react'
+
 const YoutubeTest: NextPage = () => {
+  const apikey = String(process.env.NEXT_PUBLIC_YOUTUBE_APIKEY)
+  // const apikey = String(process.env.NEXT_PUBLIC_YOUTUBE_APIKEY2)
+  // const channelID = 'UCBL4qbfyteUA-KGj3_9G1LA'
+  // const channelID = 'KIYOisGOD'
+  const channelID = 'UCZf__ehlCEBPop-_sldpBUQ' // HikakinTV
+
+  const [videos, setVideos] = useState<string[]>([''])
+  const [word, setWord] = useState<string>('')
+  const [searchWord, setSearchWord] = useState<string>('にゃんこ')
+  const [order, setOrder] = useState<string>('viewCount')
+
+  const search_api_url = 'https://www.googleapis.com/youtube/v3/search?'
+  const search_channel_url = 'https://www.googleapis.com/youtube/v3/channel?'
+
+  useEffect(() => {
+    const params = {
+      part: 'snippet',
+      key: apikey,
+      channelId: channelID,
+      // type: 'channel', // video, channel, playlist
+      type: 'video',
+      maxResults: '3', // 取得数
+      order: order, // 再生数順
+    }
+    const queryParams = new URLSearchParams(params)
+    fetch(search_api_url + queryParams)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log('API Hikakin:', result)
+          if (result.items && result.items.length !== 0) {
+            const videosId = result.items.map((v, i) => {
+              return v.id.videoId
+            })
+            setVideos(videosId)
+          }
+        },
+        (error) => {
+          console.error('err=>', error)
+        }
+      )
+  }, [order, apikey])
+
+  // useEffect(() => {
+  //   const params = {
+  //     key: apikey,
+  //     q: 'にゃんこ' + searchWord, // 検索ワード
+  //     type: 'video',
+  //     maxResults: '5', // 取得数
+  //     order: 'viewCount', // 再生数順
+  //   }
+  //   const queryParams = new URLSearchParams(params)
+  //   fetch(search_api_url + queryParams)
+  //     .then((res) => res.json())
+  //     .then(
+  //       (result) => {
+  //         console.log('API success:', result.items)
+  //         if (result.items && result.items.length !== 0) {
+  //           const videosId = result.items.map((v, i) => {
+  //             return v.id.videoId
+  //           })
+  //           setVideos(videosId)
+  //         }
+  //       },
+  //       (error) => {
+  //         console.error('err=>', error)
+  //       }
+  //     )
+  // }, [searchWord, apikey])
+
+  const onSearch = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      setSearchWord(word)
+    },
+    [word]
+  )
+  const changeWord = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setWord(e.target.value)
+  }, [])
   return (
     <>
       <Link href='/'>index</Link>
-      <div></div>
+      <div className='flex gap-3 flex-wrap'>
+        <div onClick={() => setOrder('viewCount')}>再生回数順</div>
+        <div onClick={() => setOrder('rating')}>いいね順</div>
+        <div onClick={() => setOrder('data')}>新着順</div>
+        <div onClick={() => setOrder('relevance')}>関連性順</div>
+        <div onClick={() => setOrder('title')}>アルファベット順</div>
+        <div onClick={() => setOrder('videoCount')}>アップロード順</div>
+      </div>
+      <div>
+        {videos &&
+          videos.map((v, i) => (
+            <div key={i}>
+              <iframe
+                id='player'
+                width='640'
+                height='360'
+                src={'https://www.youtube.com/embed/' + v}
+                frameBorder='0'
+                allowFullScreen
+              />
+            </div>
+          ))}
+      </div>
     </>
   )
 }
